622124e1e25953bf41228ef098a1a1eda2efa49d
Adewale Adeleye <Adewagold@yahoo.com>
2020-12-04 14:22:32 +0100

    Update requirement

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+import io
+import csv
 
 app = Flask(__name__)
 print(__name__)
+
 
 @app.route("/")
 def hello_world_default():
@@ -18,4 +21,33 @@ def hello_world_name(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    with open("database.txt", "a") as database:
+        database.write(f"\n{email}, {subject}, {message}")
 
+
+def write_to_csv(data):
+    header = ["Email", "Subject", "Message"]
+    email = data["email"]
+    subject = data["subject"]
+    message = data["message"]
+    with open("database.csv", "a") as database2:
+        csv_writer = csv.writer(database2, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route("/submit_form", methods=["GET", "POST"])
+def submit_form(email=None):
+    if request.method == "POST":
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect("thankyou.html")
+        except:
+            return "Cannot save to the database at this time."
+    else:
+        return "Something went wrong. Try again!"
